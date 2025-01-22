353927ef7ab623a1cddab353179e449486903394
zagir <zagirnur@gmail.com>
2025-01-22 22:53:46 +0500

    v0.0.9 push sdk

--- src/main/java/dev/zagirnur/petbot/sdk/TelegramBotAutoConfiguration.java
+++ src/main/java/dev/zagirnur/petbot/sdk/TelegramBotAutoConfiguration.java
@@ -5,10 +5,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * Автоконфигурация бота.
- * Будет создавать основной бин для взаимодействия с Telegram.
- */
+
 @Configuration
 @EnableConfigurationProperties(TelegramBotProperties.class)
 public class TelegramBotAutoConfiguration {
